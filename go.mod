57b368b21a4e4dc300604371969ad6d0fcd1d753
Davieas <davialexandresantana@gmail.com>
2024-04-05 19:37:57 -0300

    feat: add lcd to show values of sensor

--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module automatized-garden-go
 
 go 1.22.0
 
-require github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+require (
+	github.com/d2r2/go-hd44780 v0.0.0-20181002113701-74cc28c83a3e
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+)
 
-require golang.org/x/sys v0.19.0 // indirect
+require (
+	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc // indirect
+	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"time"
-
+	"github.com/d2r2/go-hd44780"
 	"github.com/tarm/serial" // Biblioteca para comunicação serial
 )
 
@@ -26,13 +26,19 @@ func main() {
 	garden := &Garden{}
 
 	// Configuração da comunicação serial com o Arduino
-	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
+	c := &serial.Config{Name: "COM4", Baud: 9600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		fmt.Println("Erro ao abrir porta serial:", err)
 		return
 	}
 	defer s.Close()
+
+	lcd, err := hd44780.NewLcd(2, 16, hd44780.LCD_5x8DOTS, serialPort, 0x27, hd44780.HD44780_4BITMODE)
+	if err != nil {
+		log.Fatalf("Erro ao inicializar o LCD: %v", err)
+	}
+	defer lcd.Close()
 
 	for {
 		// Comando Firmata enviado para Arduino
@@ -49,9 +55,13 @@ func main() {
 		// valor do sensor
 		sensorValue := int(buffer[0])
 
+		lcd.Clear()
+		lcd.SetCursorPosition(0, 0)
+		lcd.Print(fmt.Sprintf("Umidade do solo: %d", sensorValue))
+
 		time.Sleep(1 * time.Second)
 
-		// verificação valor do sensor 
+		// verificação valor do sensor
 		if sensorValue == 85 {
 			garden.water()
 			_, err := s.Write([]byte("L"))
@@ -65,6 +75,7 @@ func main() {
 				fmt.Println("Erro ao enviar comando para desligar a bomba de água:", err)
 			}
 		}
+		
 		time.Sleep(10 * time.Second)
 	}
 }
